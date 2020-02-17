5441666b942bed605241eac612f6b3133ad44997
Stephen <stcolema@tcd.ie>
2020-02-17 16:12:28 +0000

    Arguments pass to sub-funcitons correctly

--- R/plotComparisonCorrelationPSMData.R
+++ R/plotComparisonCorrelationPSMData.R
@@ -13,48 +13,47 @@ plotComparisonCorrelationPSMData <- function(compare_tibble,
                                              sim_breaks = NULL,
                                              expr_breaks = NULL,
                                              show_row_labels = FALSE) {
-  
   if (is.null(expr_breaks)) {
     expr_breaks <- defineBreaks(col_pal_expr)
   }
-  
-  if (is.null(sim_breaks)){
+
+  if (is.null(sim_breaks)) {
     sim_breaks <- defineBreaks(col_pal_sim)
   }
-  
+
   # The directory we will save the plots to
   loc_dir <- paste0(file_path, "Comparison_expression_clustering_correlation/")
-  
+
   # Create the directroy if it's does not already exist
   dir.create(loc_dir, showWarnings = FALSE)
-  
+
   # Loop over the datasets creating a grid of the heatmap of the similarity
   # matrix beside that of the expression data in the same row order
   for (i in 1:num_datasets) {
-    
+
     # Find the name of the current dataset
     curr_dataset <- dataset_names[[i]]
-    
+
     # Add this to the plot title
     curr_title <- paste0(curr_dataset, ": ", gen_title)
-    
+
     # The name under which the plot will be saved for this dataset
     comp_plot_name <- paste0(loc_dir, curr_dataset, file_type)
-    
+
     # Extract the relevant parts of the tibble
     curr_sim <- compare_tibble$similarity_matrix[compare_tibble$dataset == curr_dataset][[1]]
     curr_expr <- compare_tibble$expression_data[compare_tibble$dataset == curr_dataset][[1]]
-    curr_corr <- compare_tibble$correlation_matrix[compare_tibble$dataset == curr_dataset][[1]] 
+    curr_corr <- compare_tibble$correlation_matrix[compare_tibble$dataset == curr_dataset][[1]]
 
     heatmapWrapperPSMDataCorr(curr_sim,
-                              curr_expr,
-                              curr_corr,
-                              ph_title = curr_title,
-                              save_name = comp_plot_name,
-                              col_pal_sim = col_pal_sim,
-                              col_pal_expr = col_pal_expr
+      curr_expr,
+      curr_corr,
+      ph_title = curr_title,
+      save_name = comp_plot_name,
+      col_pal_sim = col_pal_sim,
+      col_pal_expr = col_pal_expr,
+      expr_breaks = expr_breaks,
+      sim_breaks = sim_breaks
     )
   }
 }
-
-
