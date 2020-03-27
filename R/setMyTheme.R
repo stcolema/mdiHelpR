56dee9ab411d11070f1ce95147568f16f78d6b80
Stephen Coleman <stcolema@tcd.ie>
2020-03-27 05:50:30 +0000

    Corrected namespace

--- R/setMyTheme.R
+++ R/setMyTheme.R
@@ -3,10 +3,10 @@
 #' @title Set my theme
 #' @description Set the theme I personally use for slides and whatnot. This is
 #' theme_bw with the headers in a facet_wrap() set to navy with white text.
-#' @importFrom ggplot2 theme_bw theme
+#' @importFrom ggplot2 theme_bw theme element_text element_rect
 #' @export
 setMyTheme <- function() {
   ggplot2::theme_set(ggplot2::theme_bw() +
-    ggplot2::theme(strip.background = element_rect(fill = "#21677e")) +
-    ggplot2::theme(strip.text = element_text(colour = "white")))
+    ggplot2::theme(strip.background = ggplot2::element_rect(fill = "#21677e")) +
+    ggplot2::theme(strip.text = ggplot2::element_text(colour = "white")))
 }
