44221dd40cdb310afe82df7eeb017f3ea43f54c4
Stephen Coleman <stcolema@tcd.ie>
2020-05-06 18:52:45 +0100

    Can now be flexible in distance and linkage inputs.

--- R/findOrder.R
+++ R/findOrder.R
@@ -4,8 +4,10 @@
 #' @description Given a matrix, m, finds the ordering of the rows as imposed by 
 #' hclust.
 #' @param m A matrix.
+#' @param method The linkage method used by ``hclust``.
+#' @param distance The metric used by ``dist``.
 #' @return A vector of integers corresponding to the row numbers of an ordered m.
 #' @export
-findOrder <- function(m){
-  hclust(dist(m))$order
+findOrder <- function(m, method = "complete", distance = "euclidean"){
+  hclust(dist(m, method = distance), method = method)$order
 }
