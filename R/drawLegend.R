6016fead8b501fef785b041fc7a8c88ba6fb25ad
Stephen Coleman <stcolema@tcd.ie>
2020-05-08 10:52:27 +0100

    Updated dependencies

--- R/drawLegend.R
+++ R/drawLegend.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 #' @title Draw legend
-#' @importFrom grid grid.pretty rectGrob textGrob grobTree gpar
+#' @importFrom grid grid.pretty rectGrob textGrob grobTree gpar unit
 draw_legend = function(color, breaks, ...){
   color = color[!is.infinite(breaks)]
   breaks = breaks[!is.infinite(breaks)]
@@ -9,18 +9,18 @@ draw_legend = function(color, breaks, ...){
   legend <- grid::grid.pretty(range(as.vector(breaks)))
   names(legend) <- legend
   
-  height = min(unit(1, "npc"), unit(150, "bigpts"))
+  height = min(grid::unit(1, "npc"), grid::unit(150, "bigpts"))
   
   legend_pos = (legend - min(breaks)) / (max(breaks) - min(breaks))
-  legend_pos = height * legend_pos + (unit(1, "npc") - height)
+  legend_pos = height * legend_pos + (grid::unit(1, "npc") - height)
   
   breaks = (breaks - min(breaks)) / (max(breaks) - min(breaks))
-  breaks = height * breaks + (unit(1, "npc") - height)
+  breaks = height * breaks + (grid::unit(1, "npc") - height)
   
   h = breaks[-1] - breaks[-length(breaks)]
   
-  rect = grid::rectGrob(x = 0, y = breaks[-length(breaks)], width = unit(10, "bigpts"), height = h, hjust = 0, vjust = 0, gp = grid::gpar(fill = color, col = "#FFFFFF00"))
-  text = grid::textGrob(names(legend), x = unit(14, "bigpts"), y = legend_pos, hjust = 0, gp = grid::gpar(...))
+  rect = grid::rectGrob(x = 0, y = breaks[-length(breaks)], width = grid::unit(10, "bigpts"), height = h, hjust = 0, vjust = 0, gp = grid::gpar(fill = color, col = "#FFFFFF00"))
+  text = grid::textGrob(names(legend), x = grid::unit(14, "bigpts"), y = legend_pos, hjust = 0, gp = grid::gpar(...))
   
   res = grid::grobTree(rect, text)
   
